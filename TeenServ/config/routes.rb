37d4bab8951d5ca3364dde9e8ed636892b189bed
Aser Eldamaty <aser_eldamaty@hotmail.com>
2017-10-12 23:05:28 -0400

    Add very simple home route

--- TeenServ/config/routes.rb
+++ TeenServ/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # Add a root at some point once we have one
-  # root 'devise/registrations#new'
+  root 'home#index'
 end
