30b9500c098bc5f7887bee7763f02e2c4cee0e5b
Aser Eldamaty <aser_eldamaty@hotmail.com>
2017-10-26 15:27:47 -0400

    Fix broken services related bugs and issues

--- TeenServ/app/controllers/services_controller.rb
+++ TeenServ/app/controllers/services_controller.rb
@@ -3,12 +3,13 @@ class ServicesController < ApplicationController
 	before_action :find_user
 
 	def new
-		 @service = Service.new({:user_id => @user.id})
+		puts @user
+		 @service = Service.new({:user_id => current_user.id})
 	end
 	def create
 		@service = Service.new(service_params)
 		if @service.save
-			redirect_to(user_path(@user.id))
+			redirect_to(user_path(current_user.id))
 		else
 			render :new #(new_user_service_path(@user.id))
 		end
