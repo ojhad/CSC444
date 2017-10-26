654f6fd5e54e5378637c6672ce5c191df0ffd844
Aser Eldamaty <aser_eldamaty@hotmail.com>
2017-10-25 21:54:35 -0400

    Remove jobs. Move services resource

--- TeenServ/db/schema.rb
+++ TeenServ/db/schema.rb
@@ -10,19 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171024044700) do
+ActiveRecord::Schema.define(version: 20171026013749) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "jobs", force: :cascade do |t|
-    t.string "title"
-    t.decimal "hours"
-    t.decimal "amount"
-    t.datetime "date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "reviews", force: :cascade do |t|
     t.integer "rating"
