956da8a4b8547464fb158bbce0ea0b9e9ed1fede
Ziyad <ziyadhelali@gmail.com>
2017-10-27 00:04:59 -0400

    Added reviews logic to user

--- TeenServ/app/controllers/reviews_controller.rb
+++ TeenServ/app/controllers/reviews_controller.rb
@@ -1,5 +1,30 @@
 class ReviewsController < ApplicationController
-  def index
+  before_action :find_user
 
+  def index
   end
+
+  def new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    if @review.save
+      redirect_to(user_path(@user.id))
+    else
+      render :new
+    end
+  end
+  private
+
+  def find_user
+    if params[:user_id]
+      @user = User.find(params[:user_id])
+    end
+  end
+
+  def review_params
+    params.require(:review).permit(:rating,:body,:user_id)
+  end
+
 end
--- TeenServ/db/schema.rb
+++ TeenServ/db/schema.rb
@@ -10,17 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171026013749) do
+ActiveRecord::Schema.define(version: 20171027020226) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "reviews", force: :cascade do |t|
-    t.integer "rating"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.float "rating", default: 5.0
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
