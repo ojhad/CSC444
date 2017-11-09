a37665bc0559ef03047d5d6810d303b74e3bbc7e
Shamitra Rohan <shamitra.rohan@gmail.com>
2017-11-08 21:58:20 -0500

    Media queries for homepage in separate file, changed icon for search

--- TeenServ/test/controllers/users_controller_test.rb
+++ TeenServ/test/controllers/users_controller_test.rb
@@ -2,41 +2,49 @@
 #
 # Table name: users
 #
-#  id                       :integer          not null, primary key
-#  email                    :string           default(""), not null
-#  encrypted_password       :string           default(""), not null
-#  reset_password_token     :string
-#  reset_password_sent_at   :datetime
-#  remember_created_at      :datetime
-#  sign_in_count            :integer          default(0), not null
-#  current_sign_in_at       :datetime
-#  last_sign_in_at          :datetime
-#  current_sign_in_ip       :inet
-#  last_sign_in_ip          :inet
-#  created_at               :datetime         not null
-#  updated_at               :datetime         not null
-#  address_1                :string
-#  address_2                :string
-#  city                     :string
-#  province                 :string
-#  postal_code              :string
-#  country                  :string
-#  home_number              :string
-#  mobile_number            :string
-#  age                      :integer
-#  group                    :integer
-#  first_name               :string
-#  last_name                :string
-#  average_rating           :float
-#  latitude                 :float
-#  longitude                :float
-#  profile_pic_file_name    :string
-#  profile_pic_content_type :string
-#  profile_pic_file_size    :integer
-#  profile_pic_updated_at   :datetime
-#  stripe_id                :string
-#  balance                  :float            default(0.0)
-#  paypal                   :string
+#  id                           :integer          not null, primary key
+#  email                        :string           default(""), not null
+#  encrypted_password           :string           default(""), not null
+#  reset_password_token         :string
+#  reset_password_sent_at       :datetime
+#  remember_created_at          :datetime
+#  sign_in_count                :integer          default(0), not null
+#  current_sign_in_at           :datetime
+#  last_sign_in_at              :datetime
+#  current_sign_in_ip           :inet
+#  last_sign_in_ip              :inet
+#  created_at                   :datetime         not null
+#  updated_at                   :datetime         not null
+#  address_1                    :string
+#  address_2                    :string
+#  city                         :string
+#  province                     :string
+#  postal_code                  :string
+#  country                      :string
+#  home_number                  :string
+#  mobile_number                :string
+#  age                          :integer
+#  image_file_name              :string
+#  image_content_type           :string
+#  image_file_size              :integer
+#  image_updated_at             :datetime
+#  profile_picture_file_name    :string
+#  profile_picture_content_type :string
+#  profile_picture_file_size    :integer
+#  profile_picture_updated_at   :datetime
+#  group                        :integer
+#  first_name                   :string
+#  last_name                    :string
+#  average_rating               :float
+#  latitude                     :float
+#  longitude                    :float
+#  profile_pic_file_name        :string
+#  profile_pic_content_type     :string
+#  profile_pic_file_size        :integer
+#  profile_pic_updated_at       :datetime
+#  stripe_id                    :string
+#  balance                      :float            default(0.0)
+#  paypal                       :string
 #
 # Indexes
 #
