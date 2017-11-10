29d7872f78b9923f344d432c506cbd3b18a593d0
Shamitra Rohan <shamitra.rohan@gmail.com>
2017-11-10 02:01:32 -0500

    Added Terms of Service

--- TeenServ/app/controllers/home_controller.rb
+++ TeenServ/app/controllers/home_controller.rb
@@ -4,4 +4,8 @@ class HomeController < ApplicationController
 
   def about
   end
+
+  def terms_of_service
+
+  end
 end
--- TeenServ/config/routes.rb
+++ TeenServ/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get 'home/index'
   get "about", to: "home#about"
+  get "terms_of_service", to: "home#terms_of_service"
 
   devise_for :users, :controllers => {:registrations => "registrations"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
