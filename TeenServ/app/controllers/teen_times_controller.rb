cd8f13f5a88b1802433e2343634d77c6243ff529
Ahmed Keshk <ahmed.keshk@mail.utoronto.ca>
2017-11-12 14:58:52 -0500

    Added delete functionality for teen times

--- TeenServ/app/controllers/teen_times_controller.rb
+++ TeenServ/app/controllers/teen_times_controller.rb
@@ -2,7 +2,17 @@ class TeenTimesController < ApplicationController
   before_action :find_user
 
   def edit
-
+    @teenTime = TeenTime.find_by_sql("SELECT * FROM teen_times WHERE user_id = #{@user.id}")
+    @teenDays = TeenTime.find_by_sql("SELECT day FROM teen_times WHERE user_id = #{@user.id}  ORDER BY
+     CASE
+          WHEN day = 'Monday' THEN 1
+          WHEN day = 'Tuesday' THEN 2
+          WHEN day = 'Wednesday' THEN 3
+          WHEN day = 'Thursday' THEN 4
+          WHEN day = 'Friday' THEN 5
+          WHEN day = 'Saturday' THEN 6
+					WHEN day = 'Sunday' THEN 7
+     END ASC").uniq{|time| time.day }
   end
 
   def create
@@ -11,7 +21,15 @@ class TeenTimesController < ApplicationController
   end
 
   def destroy
+    if params[:start_time]
+      TeenTime.where(:user_id => @user.id).where(:day => params[:day]).where(:start_time => Time.parse(params[:start_time]).strftime("%H:%M")).where(:end_time => Time.parse(params[:end_time]).strftime("%H:%M")).destroy_all
+    else
+      TeenTime.where(:user_id => @user.id).where(:day => params[:day]).destroy_all
+    end
+  end
 
+  def removeDay
+    TeenTime.where(:user_id => @user.id).where(:day => params[:day]).destroy_all
   end
 
   def find_user
