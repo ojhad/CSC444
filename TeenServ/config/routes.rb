b56b184634cc1a4726c09d1c80ce8837a234cbc3
Ziyad Helali <ziyad.helali@mail.utoronto.ca>
2017-11-13 17:30:05 -0500

    Added only needed routes

--- TeenServ/config/routes.rb
+++ TeenServ/config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
     resources :payout_informations , only:[:index, :update]
     resources :transactions
     resources :endorsements
-    resources :endorsement_requests
+    resources :endorsement_requests, only:[:create]
     resources :charges , only: [:index ,:create]
   end
 
