05735a9038b6ac4e9d63f7e68f7783093d6a1034
Ziyad Helali <ziyad.helali@mail.utoronto.ca>
2017-11-13 14:10:00 -0500

    Updated annotations

--- TeenServ/db/schema.rb
+++ TeenServ/db/schema.rb
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 20171113050016) do
     t.index ["user_id"], name: "index_endorsements_on_user_id"
   end
 
-  create_table "payout_informations", id: :bigint, default: -> { "nextval('deposit_information_id_seq'::regclass)" }, force: :cascade do |t|
+  create_table "payout_informations", force: :cascade do |t|
     t.string "paypal"
     t.string "address_1"
     t.string "address_2"
@@ -45,7 +45,7 @@ ActiveRecord::Schema.define(version: 20171113050016) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.string "method", default: "check"
-    t.index ["user_id"], name: "index_deposit_information_on_user_id"
+    t.index ["user_id"], name: "index_payout_informations_on_user_id"
   end
 
   create_table "payouts", force: :cascade do |t|
@@ -53,9 +53,7 @@ ActiveRecord::Schema.define(version: 20171113050016) do
     t.string "batch_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id"
     t.string "method"
-    t.index ["user_id"], name: "index_payouts_on_user_id"
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -124,8 +122,6 @@ ActiveRecord::Schema.define(version: 20171113050016) do
     t.bigint "skill_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["skill_id"], name: "index_user_skills_on_skill_id"
-    t.index ["user_id"], name: "index_user_skills_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -169,7 +165,6 @@ ActiveRecord::Schema.define(version: 20171113050016) do
   add_foreign_key "charges", "users"
   add_foreign_key "endorsements", "users"
   add_foreign_key "payout_informations", "users"
-  add_foreign_key "payouts", "users"
   add_foreign_key "reviews", "users"
   add_foreign_key "service_users", "services"
   add_foreign_key "service_users", "users"
