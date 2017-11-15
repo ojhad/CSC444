bc83021470652fe59de0f4f96bb6a0c5cd37b230
Aser Eldamaty <aser_eldamaty@hotmail.com>
2017-11-14 22:33:30 -0500

    Add method to add notification

--- TeenServ/app/models/notification.rb
+++ TeenServ/app/models/notification.rb
@@ -1,5 +1,7 @@
 class Notification < ApplicationRecord
   belongs_to :user
+  has_one :reference_user, :class_name => "User", :foreign_key => "reference_user_id"
+  has_one :reference_service, :class_name => "Service", :foreign_key => "reference_service_id"
 
   def self.get_all_user_notifications(user)
     Notification.where(user_id: user.id)
@@ -7,6 +9,10 @@ class Notification < ApplicationRecord
 
   def self.get_unread_user_notifications(user)
     Notification.where(user_id: user.id, read: false)
+  end
+
+  def self.create_notification(user, title, reference_user=nil, reference_service=nil)
+    Notification.create(user_id: user.id, title: title, reference_user_id: reference_user.id, reference_service_id: reference_service.id)
   end
 end
 
