cffac042d66560a21145cd8155024eadc893fc97
Aser Eldamaty <aser_eldamaty@hotmail.com>
2017-11-14 21:01:39 -0500

    Annotations that were not added before

--- TeenServ/app/models/payout.rb
+++ TeenServ/app/models/payout.rb
@@ -12,5 +12,10 @@ end
 #  batch_id   :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  user_id    :integer
 #  method     :string
+#
+# Indexes
+#
+#  index_payouts_on_user_id  (user_id)
 #
