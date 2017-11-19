5b0b475dec2ff8ddda52e7ea8f85e64a3e9f9541
Shamitra Rohan <shamitra.rohan@gmail.com>
2017-11-19 01:34:13 -0500

    Added Contact Us page

--- TeenServ/app/controllers/home_controller.rb
+++ TeenServ/app/controllers/home_controller.rb
@@ -10,4 +10,12 @@ class HomeController < ApplicationController
 
   def faq
   end
+
+  def contact_us
+  end
+
+  def send_email
+    ContactUsMailer.contact_us_mail(params[:request_anonymous_requester_email], params[:request_description]).deliver
+    redirect_to root_path
+  end
 end
--- TeenServ/config/routes.rb
+++ TeenServ/config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get "about", to: "home#about"
   get "terms_of_service", to: "home#terms_of_service"
   get "faq", to: "home#faq"
+  get "contact_us", to: "home#contact_us"
+  get "send_email", to: "home#send_email"
 
   devise_for :users, :controllers => {:registrations => "registrations"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
