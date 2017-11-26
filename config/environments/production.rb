00b6266e3803aa303254df589c82cf29eee2ff4b
Mohamed Alkakkali <mohamed.alkakkali@mail.utoronto.ca>
2017-11-26 16:27:08 -0500

    change back ALL changes to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,9 +27,6 @@ Rails.application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
-  config.cache_classes = true
-  config.serve_static_assets = true
-  config.assets.digest = true
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
