e07ffe415dd15e901c99264a6dd7f8032e6ade02
Aser Eldamaty <aser_eldamaty@hotmail.com>
2017-11-26 12:33:54 -0500

    Annotations

--- test/controllers/payouts_controller_test.rb
+++ test/controllers/payouts_controller_test.rb
@@ -7,7 +7,12 @@
 #  batch_id   :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  user_id    :integer
 #  method     :string
+#
+# Indexes
+#
+#  index_payouts_on_user_id  (user_id)
 #
 
 require 'test_helper'
