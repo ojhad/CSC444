a970a0f9ec1d548499697de479b1a914d22c7356
Aser Eldamaty <aser_eldamaty@hotmail.com>
2017-11-26 13:41:55 -0500

    Add scopes and relations for users, convo, message

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -3,6 +3,13 @@ class Conversation < ApplicationRecord
   belongs_to :recipient, :class_name => 'User'
 
   has_many :messages, dependent: :destroy
+
+  validates_uniqueness_of :sender_id, :scope => :recipient_id
+
+  scope :between, -> (sender_id, recipient_id) do
+    where("(conversations.sender_id = #{sender_id} AND conversations.recipient_id = #{recipient_id})
+      OR (convserations.sender_id = #{recipient_id} AND conversations.recipient_id = #{sender_id})")
+  end
 end
 
 # == Schema Information
