96f25e1ea92b417183762e73cb967ed43cdcc129
Ahmed Keshk <ahmed.keshk@mail.utoronto.ca>
2017-11-29 02:32:32 -0500

    removed recommended services when user is busy

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,9 +4,21 @@ class HomeController < ApplicationController
       @user = User.find(current_user.id)
       if @user.is_teen?
 
-        @recommended_services = Service.find_by_sql("SELECT S.ID,S.TITLE,S.CHARGE_PER_HOUR,S.FREQUENCY,S.DESCRIPTION FROM SERVICES S JOIN
+        @recommended_services = Service.find_by_sql("SELECT S.ID,S.TITLE,S.CHARGE_PER_HOUR,S.FREQUENCY,S.DESCRIPTION,S.DATE,S.START_TIME,S.END_TIME,S.DAY FROM SERVICES S JOIN
   (SELECT U.SKILL_ID,U.USER_ID,DAY,START_TIME,END_TIME FROM USER_SKILLS U JOIN TEEN_TIMES T ON (U.USER_ID=T.USER_ID AND U.USER_ID=#{@user.id})) A
     ON (S.STATUS=1 AND S.SKILL=A.SKILL_ID AND (#{@user.age} BETWEEN MIN_AGE AND MAX_AGE) AND S.DAY = A.DAY  AND S.START_TIME>=A.START_TIME AND S.END_TIME<=A.END_TIME)")
+
+        @busy_times = Service.find_by_sql("SELECT date, start_time, end_time, day FROM services s JOIN service_users su
+        ON s.id = su.service_id AND su.user_id = #{@user.id} AND s.status = 2")
+
+        @recommended_services.each_with_index do |rs, index|
+          @busy_times.each do |bt|
+            if rs.date.to_date == bt.date.to_date && rs.start_time <= bt.end_time && rs.end_time >= bt.start_time
+              @recommended_services.delete_at(index)
+              break
+            end
+          end
+        end
 
         @user_skills = UserSkill.where(user_id: @user.id)
         @user_availability =  TeenTime.where(user_id: @user.id)
