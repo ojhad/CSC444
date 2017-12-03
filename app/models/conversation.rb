03dcb37db1996cc84dec2836a5ba097e8694b261
Aser Eldamaty <aser_eldamaty@hotmail.com>
2017-12-03 18:12:02 -0500

    Add total unread messages count

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -6,6 +6,16 @@ class Conversation < ApplicationRecord
 
   validates_uniqueness_of :sender_id, :scope => :recipient_id
 
+  def self.my_unread_messages_count (user_id)
+    conversations = Conversation.user_conversations(user_id)
+    unread_count = 0
+    conversations.each do |conversation|
+      user_to_find = conversation.sender_id == user_id ? conversation.recipient_id : conversation.sender_id
+      unread_count += conversation.messages.unread_by_me(user_to_find).count
+    end
+    return unread_count
+  end
+
   scope :between, -> (sender_id, recipient_id) do
     where("(conversations.sender_id = #{sender_id} AND conversations.recipient_id = #{recipient_id})
       OR (conversations.sender_id = #{recipient_id} AND conversations.recipient_id = #{sender_id})")
