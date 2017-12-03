faa58c2badda39dc3c0c4d25a08dcbfbe6aa6336
Aser Eldamaty <aser_eldamaty@hotmail.com>
2017-12-03 17:24:01 -0500

    Add message when service is requested.

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -190,6 +190,10 @@ AND B.start_time<='#{@service.start_time}' AND B.END_TIME>='#{@service.end_time}
                                          user_id: @service.user_id,
 																				 notification_type: "AddRequest",
                                          read: FALSE
+			Message.create body: "This is an auto generated message. Refer to your notifications for more information! #{current_user.first_name} #{current_user.last_name} has requested you for #{@service.title}!",
+										 read: FALSE,
+										 conversation_id: Conversation.between(current_user.id, @service.user_id).first.id,
+										 user_id: current_user.id
       #@service.user.notifications.create_notification(@service.user,
        # "#{current_user.first_name} #{current_user.last_name} has requested you for #{@service.title}!",
         #current_user, @service)
