ab172b62906360990697e4b9edc5a77dd42e00c2
ojhad <dilip.ojha@mail.utoronto.ca>
2017-12-03 18:12:46 -0500

    more tests

--- features/step_definitions/home_page_steps.rb
+++ features/step_definitions/home_page_steps.rb
@@ -5,3 +5,15 @@ end
 Then(/^I should see the Getting Started link$/) do
   expect(page).to have_content("Get started")
 end
+
+Then(/^I should see the english language option$/) do
+  expect(page).to have_content("English")
+end
+
+Then(/^I should see the french language option$/) do
+  expect(page).to have_content("Français")
+end
+
+Then(/^I should see the services option$/) do
+  expect(page).to have_content("Services")
+end
--- spec/models/service_spec.rb
+++ spec/models/service_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Service, type: :model do
-  it { is_expected.to validate_presence_of(:status) }
-  it { is_expected.to validate_presence_of(:charge_per_hour) }
+  it { should validate_presence_of(:status) }
+  it { should validate_presence_of(:charge_per_hour) }
+  it { should validate_numericality_of(:charge_per_hour) }
+  it { should validate_presence_of(:user_id) }
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:min_age) }
+  it { should validate_numericality_of(:min_age) }
+  it { should validate_presence_of(:max_age) }
+  it { should validate_numericality_of(:max_age) }
 end
