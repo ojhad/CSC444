a47838e343cb594024fee074aab588b4947fe6cc
Ahmed Keshk <ahmed.keshk@mail.utoronto.ca>
2017-12-03 23:19:23 -0500

    Fixed map bug for age filter too

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -69,7 +69,7 @@ class ServicesController < ApplicationController
       newCard = "<div class = 'card-container'>" << "<div class = 'card transaction-card sjobs' data-serviceId='#{s.id}'>"
   	  cardBody = "<div class = 'card-body'>"
       
-      cardBody << "<%= image_tag('#{s.get_image_id}service.svg', width = '32', height = '32', class = 'service-image' )%>"
+      cardBody << "image_tag('#{s.get_image_id}service.svg', width = '32', height = '32', class = 'service-image' )"
       cardBody << "<h4 class = 'card-title'>#{s.main_title}</h4><br>" 
       cardBody << "<p class = 'card-title'>Posted By: <a href = '/users/#{s.user.id}''>#{ s.user.first_name.capitalize} #{s.user.last_name.capitalize}</a></p>"
       cardBody << "<p class='card-text'>Charge: $#{s.charge_per_hour} #{t :perHour}</p>"
@@ -81,6 +81,13 @@ class ServicesController < ApplicationController
 
       newCard << cardBody << "</div>" << "</div>"
 			container << newCard
+    end
+
+    @servicesLocations = Gmaps4rails.build_markers(@services) do |service, marker|
+      marker.lat service.latitude
+      marker.lng service.longitude
+      marker.infowindow  service.main_title
+      marker.json({ :id => service.id })
     end
 
    	respond_to do |format|
