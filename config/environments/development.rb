9d9999a8c212498fc17d35aea15d9b9ab878acd3
Aser Eldamaty <aser_eldamaty@hotmail.com>
2017-12-03 20:52:34 -0500

    Fix reset password email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,12 +26,30 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  config.action_mailer.delivery_method = :letter_opener
+  # config.action_mailer.delivery_method = :letter_opener
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.perform_caching = false
+  # config.action_mailer.perform_caching = false
+
+
+
+  #Mailer stuff
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+  ActionMailer::Base.smtp_settings =
+      {
+          :address => "smtp.gmail.com",
+          :port => 587,
+          :authentication => :plain,
+          :domain => 'gmail.com',
+          :user_name => 'teenserv7@gmail.com',
+          :password => 'offtherails'
+      }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
