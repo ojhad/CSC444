dde61e9106f36a66ab496c3f558d45a6f927b47f
ojhad <dilip.ojha@mail.utoronto.ca>
2017-12-04 14:13:13 -0500

    added two more tests

--- spec/Authentication/sign_in.rb
+++ spec/Authentication/sign_in.rb
@@ -8,4 +8,22 @@ feature 'Sign In' do
     end
     expect(page).to have_content('Invalid Email or password.')
   end
+
+  scenario 'Sign in attempt with password field empty should return error' do
+    visit '/users/sign_in'
+    fill_in 'user_email', with: 'client1@test.com'
+    within '.button-container' do
+      click_on(class: "login-btn")
+    end
+    expect(page).to have_content('Invalid Email or password.')
+  end
+  scenario 'Sign in attempt with valid credentials should succeed' do
+    visit '/users/sign_in'
+    fill_in 'user_email', with: 'client1@test.com'
+    fill_in 'user_password', with: 'testtest'
+    within '.button-container' do
+      click_on(class: "login-btn")
+    end
+    expect(page).to have_content('Welcome')
+  end
 end
